1049b5be613601d9ae6d673976aad0ab36ce309d
tuandtvt88 <tuandtvt88@gmail.com>
2025-03-31 13:50:49 +0700

    update14

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,14 @@ import { Outlet } from "react-router-dom";
 import './Main.css'
 export function Main({ children }) {
     return (
-      <div className="main">
+      <div className="main"
+      style={{
+                        backgroundImage: `url(${require("../assets/Toancanhtruong.jpg")})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        minHeight: "80vh"
+                    }}
+                    >
         <Outlet/>
       </div>
     );
